b0bc782bb97fd01197c5571597beb7c6374c2e35
Ahmad Sakr <ahmad.mhmd.sakr@gmail.com>
2023-06-27 00:30:17 +0300

    Queue initial commit

--- queue/queue_api.go
+++ queue/queue_api.go
@@ -1,1 +1,55 @@
+// Package queue stores different queue implementations
 package queue
+
+// Basic interface for queues that is confined to
+// FIFO data operations.
+type Basic[T any] interface {
+	// Enqueue stores a datum at the tail of the
+	// queue.
+	//
+	// Enqueue returns an error if and only if
+	// the queue is full, otherwise it returns
+	// nil.
+	EnQueue(datum T) error
+
+	// Dequeue removes the first element from
+	// the queue.
+	//
+	// Dequeue returns an error if and only if
+	// the queue is empty, otherwise it returns
+	// nil.
+	DeQueue() error
+
+	// Front sets the variiable pointed to by dst
+	// equal to that stored at the front of the
+	// queue.
+	//
+	// Front returns an error if and only if the
+	// queue is empty (with no change to dst's
+	// value), otherwise it returns nil.
+	Front(dst *T) error
+}
+
+// Extended interface for queue that includes FIFO
+// data operations with additional state queries.
+type Extended[T any] interface {
+	// IsEmpty returs true if the queue is empty,
+	// otherwise false.
+	IsEmpty() bool
+
+	// IsFull return true if the queue has reached
+	// its full capacity, otherwise false.
+	IsFull() bool
+
+	// Size returns the count of elements currently
+	// stored in the queue.
+	//
+	// If the queue is empty then the size of the queue
+	// is 0. If the queue is full then the size of the
+	// queue equals its capacity.
+	Size() uint32
+
+	// Capacity returns the maximum amount of elements a
+	// queue can store at once.
+	Capacity() uint32
+}
--- queue/queue_circular.go
+++ queue/queue_circular.go
@@ -1,1 +1,52 @@
 package queue
+
+import (
+	"fmt"
+)
+
+// Creates a static queue instance satisfying the
+// [queue.Extended] interface.
+func NewStatic[T any](capacity uint32) Extended[T] {
+	return &circular[T]{
+		front:     uint32(0),
+		size:      uint32(0),
+		capacity:  capacity,
+		container: make([]T, capacity),
+	}
+}
+
+type circular[T any] struct {
+	front     uint32
+	size      uint32
+	capacity  uint32
+	container []T
+}
+
+func (q *circular[T]) IsEmpty() bool    { return q.size == 0 }
+func (q *circular[T]) IsFull() bool     { return q.size == q.capacity }
+func (q *circular[T]) Size() uint32     { return q.size }
+func (q *circular[T]) Capacity() uint32 { return q.capacity }
+func (q *circular[T]) EnQueue(datum T) error {
+	if q.IsFull() {
+		return fmt.Errorf("queue is full! size = %v  have reached capacity = %v", q.Size(), q.Capacity())
+	}
+	index := (q.front + q.size) % q.capacity
+	q.container[index] = datum
+	q.size++
+	return nil
+}
+func (q *circular[T]) DeQueue() error {
+	if q.IsEmpty() {
+		return fmt.Errorf("queue is empty! %v elements are in the queue", q.Size())
+	}
+	q.front = (q.front + 1) % q.capacity
+	q.size--
+	return nil
+}
+func (q *circular[T]) Front(dst *T) error {
+	if q.IsEmpty() {
+		return fmt.Errorf("queue is empty! %v elements are in the queue", q.Size())
+	}
+	*dst = q.container[q.front]
+	return nil
+}
